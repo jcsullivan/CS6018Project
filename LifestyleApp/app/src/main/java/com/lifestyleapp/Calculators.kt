60cbe3da5f2ee4aba3dcbc0e906719f4daa628bc
Jonathan <jonathan.c.sullivan@gmail.com>
2020-09-03 11:32:53 -0600

    Calculators completed in Kotlin.

--- LifestyleApp/app/src/main/java/com/lifestyleapp/Calculators.kt
+++ LifestyleApp/app/src/main/java/com/lifestyleapp/Calculators.kt
@@ -2,23 +2,57 @@
 
 package com.example.lifestyleapp
 
+// Calling Kotlin from Java:  https://kotlinlang.org/docs/reference/java-to-kotlin-interop.html 
 class Calculators
 {
     fun BMI(height: Int, weight: Int): Int
     {
+        // Equation from https://en.wikipedia.org/wiki/Body_mass_index
+        val bmi = (703 * weight) / (height * height);
 
-        return 0;
+        return bmi;
     }
 
-    fun BMR(gender: String, height: Int, weight: Int, age: Int, active: Boolean): Int
+    fun BMRTEE(gender: String, height: Int, weight: Int, age: Int, active: Boolean): Double
     {
+        // Converting weight to kilograms.
+        var kgWeight = weight / 2.205;
+        // Converting height to centimeters.
+        var cmHeight = height * 2.54;
 
-        return 0;
+        // Equations from https://en.wikipedia.org/wiki/Harris%E2%80%93Benedict_equation
+        var genderlessBMR = (10 * kgWeight) + (6.25 * cmHeight) - (5 * age);
+
+        var bmr = 0.0;
+
+        if (gender == "Male")
+        {
+            bmr = genderlessBMR + 5;
+        }
+        else
+        {
+            bmr = genderlessBMR - 161;
+        }
+
+        var tee = 0.0;
+
+        if(active)
+        {
+            tee = bmr * 1.76;
+        }
+        else
+        {
+            tee = bmr * 1.53;
+        }
+
+        return tee;
     }
 
-    fun caloriesToEat(desiredChange: Int, BMR: Int): Int
+    fun caloriesToEat(desiredChange: Double, tee: Double): Double
     {
+        // To reduce 1 kg (2.205 lbs) of weight, about 7000 kcal deficit is required.
+        var dailyCalories = (1000 / (desiredChange / 2.205)) + tee;
 
-        return 0;  
+        return dailyCalories;
     }
 }
