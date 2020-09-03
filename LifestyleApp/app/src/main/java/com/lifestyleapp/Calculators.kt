32bb3edfb0a854bd0861e766476353382b888b17
Jonathan <jonathan.c.sullivan@gmail.com>
2020-09-03 08:39:25 -0600

    Initial commit for Lifestyle App.

---
+++ LifestyleApp/app/src/main/java/com/lifestyleapp/Calculators.kt
@@ -0,0 +1,24 @@
+//@file:JvmName("Calcs")
+
+package com.example.lifestyleapp
+
+class Calculators
+{
+    fun BMI(height: Int, weight: Int): Int
+    {
+
+        return 0;
+    }
+
+    fun BMR(gender: String, height: Int, weight: Int, age: Int, active: Boolean): Int
+    {
+
+        return 0;
+    }
+
+    fun caloriesToEat(desiredChange: Int, BMR: Int): Int
+    {
+
+        return 0;  
+    }
+}
