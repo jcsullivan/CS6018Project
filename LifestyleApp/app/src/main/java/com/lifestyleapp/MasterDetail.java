0bef3d286019bc2d9954f177d1807464ea148418
Sam <samuelbauter@gmail.com>
2020-09-18 20:27:27 -0600

    Quick Fix For phone

--- LifestyleApp/app/src/main/java/com/lifestyleapp/MasterDetail.java
+++ LifestyleApp/app/src/main/java/com/lifestyleapp/MasterDetail.java
@@ -55,7 +55,7 @@ public class MasterDetail extends AppCompatActivity implements NavPaneFragment.O
             if(navIndex==mMasterListNavFrag.PROFILE_BUTTON_INDEX){
                 fTrans.replace(R.id.master_detail_pane_phone,profilePageFragment,"frag_myprof_phone");
             }else if (navIndex==mMasterListNavFrag.WEIGHT_BUTTON_INDEX){
-                fTrans.replace(R.id.master_detail_right_pane_tablet,weightManFragment,"frag_weightman_phone");
+                fTrans.replace(R.id.master_detail_pane_phone,weightManFragment,"frag_weightman_phone");
 
             }
         }
