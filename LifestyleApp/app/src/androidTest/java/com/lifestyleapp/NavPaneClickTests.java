65e735f3495623d1a15d0f50c0bace5730de98c9
Sam <samuelbauter@gmail.com>
2020-09-29 15:20:12 -0600

    More Instrumented Tests

--- LifestyleApp/app/src/androidTest/java/com/lifestyleapp/NavPaneClickTests.java
+++ LifestyleApp/app/src/androidTest/java/com/lifestyleapp/NavPaneClickTests.java
@@ -1,5 +1,6 @@
 package com.lifestyleapp;
 
+
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -7,23 +8,49 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
+
 import static org.hamcrest.Matchers.*;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
+
+import java.util.List;
+
+
 @RunWith(AndroidJUnit4.class)
-public class MasterDetailClickTests {
+public class NavPaneClickTests {
     @Rule
     public ActivityScenarioRule<MasterDetail> masterActRule = new ActivityScenarioRule<>(MasterDetail.class);
+
 
     @Test
     public void clickMyProf(){
         ViewInteraction moveToMyProfBtn = onView(withId(R.id.my_prof_btn_frag));
         moveToMyProfBtn.perform(click());
         onView(withId(R.id.title_my_prof_frag)).check(matches(withText(containsString("MY PROFILE"))));
-
     }
+
+    @Test
+    public void clickWeightMan(){
+        ViewInteraction moveToWeightManBtn = onView(withId(R.id.weight_man_btn_frag));
+        moveToWeightManBtn.perform(click());
+        onView(withId(R.id.title_weight_man_frag)).check(matches(withText(containsString("WEIGHT MANAGEMENT CALCULATOR"))));
+    }
+
+    @Test
+    public void clickLocalWeather(){
+        ViewInteraction moveToWeatherBtn = onView(withId(R.id.weather_btn_frag));
+        moveToWeatherBtn.perform(click());
+        onView(withId(R.id.resetLocation)).check(matches(withText(containsString("Set Location"))));
+    }
+
+
+
+
+
+
+
 
 
 
--- LifestyleApp/app/src/test/java/com/lifestyleapp/WeatherUtilitiesTests.java
+++ LifestyleApp/app/src/test/java/com/lifestyleapp/WeatherUtilitiesTests.java
@@ -11,7 +11,7 @@ import static com.lifestyleapp.WeatherUtilities.getDataFromURL;
 import static com.lifestyleapp.WeatherUtilities.parseJSON;
 import static org.junit.Assert.*;
 
-public class WeatherUtilitiesTest {
+public class WeatherUtilitiesTests {
 
 
     @Test
