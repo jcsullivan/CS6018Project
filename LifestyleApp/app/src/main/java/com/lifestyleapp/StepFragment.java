8aeff502c481a1a80bcdbe365b3663613b127aaa
Jonathan <jonathan.c.sullivan@gmail.com>
2020-10-17 10:21:58 -0600

    Initial commit

---
+++ LifestyleApp/app/src/main/java/com/lifestyleapp/StepFragment.java
@@ -0,0 +1,15 @@
+package com.lifestyleapp;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class StepFragment extends Fragment implements View.OnClickListener
+{
+    TextView counterField;
+    Button startSteps, stopSteps, stepBackToLifestyle;
+
+    
+}
